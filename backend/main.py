ce381258db119acb32e3b6b273e19b262153c7f6
rajimaaa <rajmasmud12@gmail.com>
2025-10-08 07:04:59 +0800

    pls

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,11 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "https://gamehubjiji-044p.onrender.com",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
